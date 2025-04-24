4e3380d1510e936527b58c06be30f983a8860d3d
Jack Burton <jackb898@gmail.com>
2025-04-24 17:17:47 -0400

    Update Match_detect.js

--- Match_detect.js
+++ Match_detect.js
@@ -1,16 +1,29 @@
+/* Match_detect
+   Author: Jack Burton
+   April 2025
+   
+   Continuously pulls UART responses from fingerprint sensor, checking
+   for signature indicating successful Search, then enables DIO6 for 5sec.
+   Requirements:
+    UART Spy (Protocol tab) enabled with TX = DIO 1, RX = PIO0
+    Baud rate 57.6k
+    Parity: None, 8 Bits, Polarity: Standard
+    Wavegen1 open with appropriate voltage for whatever you're powering (i.e. 2V for LED)
+*/
+
 if (!('Protocol' in this)) throw "Please open the Protocol tool";
 
 Protocol.Mode.text = "UART"; // Ensure UART mode is selected
 Protocol.UART.Receiver();    // Reset UART receiver
 
+// Continuously check for packets
 while(wait(0.1)) {
-    // Define the target pattern in decimal
-    var target = [7, 0, 7, 0];
+    var target = [7, 0, 7, 0]; // Target signature, indicates successful Search (which happens during matching process)
     
-    // Read received data
-    var rx = Protocol.UART.ReceiveArray();
+
+    var rx = Protocol.UART.ReceiveArray(); // Read received data
     
-    // Print received data as hex manually
+    // Print received data in hex format
     var hexStr = "";
     for (var i = 0; i < rx.length; i++) {
         var hex = rx[i].toString(16).toUpperCase();
@@ -29,7 +42,7 @@ while(wait(0.1)) {
                 break;
             }
         }
-        if (found) {
+        if (found) { // End loop temporarily if matched fingerprint
             print("match found!\n")
             match = true;
             break;
